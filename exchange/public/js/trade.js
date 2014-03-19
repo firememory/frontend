5817c499e156a945df74cf1d4fde14c49ad33f01
LiuChunming <chunming02@163.com>
2014-03-19 13:03:47 +0800

    modify UI

--- exchange/public/js/trade.js
+++ exchange/public/js/trade.js
@@ -14,6 +14,15 @@ function routeConfig($routeProvider) {
         controller: 'DepositBtcCtrl',
         templateUrl: 'views/deposit-btc.html'
     }).
+    when('/account', {
+            controller: 'UserCtrl',
+            templateUrl: 'views/account.html'
+    }).
+
+    when('/test', {
+            controller: 'DepositBtcCtrl',
+            templateUrl: 'views/test.html'
+    }).
     otherwise({
         redirectTo: '/'
     });
@@ -23,8 +32,8 @@ tradeApp.config(routeConfig);
 
 function BidAskCtrl($scope, $http) {
     $scope.orders = [];
-    $scope.bid = {type: 'bid', price: 4419, amount: 0, total: 0};
-    $scope.ask = {type: 'ask', price: 4419, amount: 0, total: 0};
+    $scope.bid = {type: 'bid', price: 4000, amount: 0, total: 0};
+    $scope.ask = {type: 'ask', price: 5000, amount: 0, total: 0};
     $scope.account = {RMB: 'loading', BTC: 'loading'}
 
     $scope.refresh = function() {
@@ -147,7 +156,8 @@ function BidAskCtrl($scope, $http) {
 
         $http.post('trade/bid', $scope.bid)
           .success(function(data, status, headers, config) {
-            console.log('bid order sent');
+            console.log('bid order sent, response:', data);
+            $scope.refresh();
         });
     };
 
@@ -166,7 +176,8 @@ function BidAskCtrl($scope, $http) {
 
         $http.post('trade/bid', $scope.ask)
           .success(function(data, status, headers, config) {
-            console.log('bid order sent');
+            console.log('bid order sent, response:', data);
+            $scope.refresh();
         });
     };
 
@@ -248,6 +259,103 @@ tradeApp.controller('DepositBtcCtrl', ['$scope', '$http', function($scope, $http
     });
 
 }]);
+
+tradeApp.controller('UserCtrl', function ($scope, $http) {
+    $http.get('api/account')
+        .success(function(data, status, headers, config) {
+            console.log('got', data);
+            $scope.accounts = data.accounts;
+        });
+    // mocked data
+    var data = [];
+
+    for (var i = 1; i < 30; i++) {
+        var time = new Date(2014, 0, i);
+        var btc = 10000 + 2000 * Math.random();
+        var ltc = 3100;
+        var pts = 1000;
+        var cny = 13411 + 5000 * Math.floor(i / 20);
+        var price = 4000 + 200 * Math.random();
+
+        data.push({date: time, value1: cny, value2: btc, value3: ltc, value4: pts, value5: price});
+    }
+
+    // jQuery code
+    $('#user-finance-chart-history').jqChart({
+        title: { text: '资产净值(CNY)' },
+        dataSource: data,
+        axes: [
+            {
+                type: 'dateTime',
+                location: 'bottom',
+                minimum: new Date(2014, 0, 1),
+                maximum: new Date(2014, 0, 29),
+                interval: 1,
+                intervalType: 'days'
+            },
+            {
+                name: 'y1',
+                location: 'left'
+            }
+        ],
+        series: [
+            {
+                type: 'stackedArea',
+                title: '人民币(CNY)',
+                xValuesField: 'date',
+                yValuesField: 'value1',
+                axisY: 'y1'
+            },
+            {
+                type: 'stackedArea',
+                title: '比特币(BTC)',
+                xValuesField: 'date',
+                yValuesField: 'value2',
+                axisY: 'y1'
+            },
+            {
+                type: 'stackedArea',
+                title: '莱特币(LTC)',
+                xValuesField: 'date',
+                yValuesField: 'value3',
+                axisY: 'y1'
+            },
+            {
+                type: 'stackedArea',
+                title: '原型股(PTS)',
+                xValuesField: 'date',
+                yValuesField: 'value4',
+                axisY: 'y1'
+            }
+        ]
+    });
+
+    $('#user-finance-chart-pie').jqChart({
+        title: { text: '资产构成' },
+        legend: {},
+        shadows: {
+            enabled: true
+        },
+        series: [
+            {
+                type: 'pie',
+                fillStyles: ['#418CF0', '#FCB441', '#E0400A', '#056492', '#BFBFBF', '#1A3B69', '#FFE382'],
+                data: [['人民币(CNY)', 15498.21], ['比特币(BTC)', 58745.04], ['莱特币(LTC)', 3031.12],
+                    ['原型股(PTS)', 6064.73]],
+                labels: {
+                    stringFormat: '%.1f%%',
+                    valueType: 'percentage',
+                    font: '15px sans-serif',
+                    fillStyle: 'white'
+                },
+                explodedRadius: 10,
+                explodedSlices: [0],
+                strokeStyle : '#cccccc',
+                lineWidth : 1
+            }
+        ]
+    });
+});
 
 tradeApp.filter('orderTypeText', function() {
     var filter = function(input) {
