fdc2ab4b788d1d6155c76ee748ac3ef456258d2f
LiuChunming <chunming02@163.com>
2014-03-21 16:56:53 +0800

    update market page

--- exchange/public/js/market.js
+++ exchange/public/js/market.js
@@ -1,5 +1,4 @@
-// Declare app level module which depends on filters, and services
-var marketApp = angular.module('coinport.market', ['ui.bootstrap', 'ngResource', 'navbar']);
+var marketApp = angular.module('coinport.market', []);
 
 marketApp.controller('MarketCtrl', function ($scope, $http) {
     $http.get('api/price')
@@ -13,51 +12,35 @@ marketApp.controller('MarketCtrl', function ($scope, $http) {
                 $scope.history.push([row[0]*1000, row[3], row[5], row[6], row[4], row[7]]);
             });
 
-            var chart = $('.candle-chart').jqCandlestick($scope.history, {
-              theme: 'light',
-              yAxis: [{
-                height: 8
-              }, {
-                height: 2
-              }],
-              info: {
-                color: '#000', // color for info
-                font: null, // font
-                spacing: 10, // distance between values
-                position: 'left', // 'left', 'right' or 'auto'
-                wrap: 'no' // 'auto', 'yes' or 'no'
-              },
-              cross: {
-                color: 'rgba(0, 0, 0, 0.6)', // color of cursor-cross
-                strokeWidth: 1.0, // width cursor-cross lines
-                text: {
-                  //background: '#cccccc', // background color for text
-                  font: null, // font for text
-                  color: '#000' // color for text
-                }
-              },
-              xAxis: {
-                dataLeftOffset: Math.max(0, $scope.history.length - 61),
-                dataRightOffset: $scope.history.length - 1
-              },
-              series: [{
-                type: 'candlestick',
-                names: ['开盘','最高', '最低', '收盘'],
-                upStroke: '#0C0',
-                downStroke: '#C00',
-                downColor: 'rgba(255, 0, 0, 0.7)'
-              }, {
-                type: 'volume',
-                name: '成交量',
-                dataOffset: 5,
-                yAxis: 1,
-                stroke: '#00C',
-                color: 'rgba(0, 0, 255, 0.7)',
-                upStroke: '#009dc6',
-                upColor: 'rgba(0, 150, 255, 0.3)',
-                downStroke: '#b9231f',
-                downColor: 'rgba(255, 0, 0, 0.3)'
-              }]
+            var chart = $('#wrapper').jqCandlestick( $scope.history, {
+                theme: 'dark',
+                yAxis: [{
+                    height: 8
+                }, {
+                    height: 2
+                }],
+                xAxis: {
+                    dataLeftOffset: data.length - 90,
+                    minDataLength: 90
+                },
+                series: [{
+                    type: 'candlestick',
+                    name: 'OHLC',
+                    upStroke: '#0C0',
+                    downStroke: '#C00',
+                    downColor: 'rgba(255, 0, 0, 0.5)'
+                }, {
+                    type: 'volume',
+                    name: 'VOLUME',
+                    yAxis: 1,
+                    dataOffset: 5,
+                    stroke: '#00C',
+                    color: 'rgba(0, 0, 255, 0.6)',
+                    upStroke: '#0C0',
+                    downStroke: '#C00',
+                    upColor: 'rgba(0, 255, 0, 0.5)',
+                    downColor: 'rgba(255, 0, 0, 0.5)'
+                }]
             });
         });
     $http.get('api/depth')
