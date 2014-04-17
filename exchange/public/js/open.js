a9bcef3a0ef4b5bcfd24d8de4101f55b4170c01c
LiuChunming <chunming02@163.com>
2014-04-17 15:03:36 +0800

    add total account to open page

--- exchange/public/js/open.js
+++ exchange/public/js/open.js
@@ -1,12 +1,18 @@
 var openApp = angular.module('coinport.open', ['ui.bootstrap', 'ngResource', 'navbar']);
 
 openApp.controller('OpenCtrl', function ($scope, $http) {
+    $http.get('/api/account/-1')
+    .success(function(data, status, headers, config) {
+        $scope.accounts = data.data.accounts;
+    });
+
     $http.get('/api/open/data/dw')
-        .success(function(data, status, headers, config) {
-            $scope.dw = data.data;
-        });
+    .success(function(data, status, headers, config) {
+        $scope.dw = data.data;
+    });
+
     $http.get('/api/open/data/mu')
-        .success(function(data, status, headers, config) {
-            $scope.mu = data.data;
-        });
+    .success(function(data, status, headers, config) {
+        $scope.mu = data.data;
+    });
 });
