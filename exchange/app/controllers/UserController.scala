a9fb2e6d73cb2fe6c89fc7b9e3ea8fdd977b2258
Kongliang Zhong <zhongkl@gmail.com>
2014-04-23 20:27:36 +0800

    fix bug: captcha exception(font words too tall), and add validation for user input

--- exchange/app/controllers/UserController.scala
+++ exchange/app/controllers/UserController.scala
@@ -13,48 +13,58 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 import com.github.tototoshi.play2.json4s.native.Json4s
 import com.coinport.coinex.data.{LoginSucceeded, UserProfile}
+import ControllerHelper._
 
 object UserController extends Controller with Json4s {
   def login = Action.async(parse.urlFormEncoded) {
     implicit request =>
     val data = request.body
-    val email = getParam(data, "username").get
-    val password = getParam(data, "password").get
-    val user: User = User(None, email, None, password, None)
-    UserService.login(user) map {
-      result =>
-      if (result.success) {
-        result.data.get match {
-          case succeeded: LoginSucceeded =>
-            Ok(result.toJson).withSession(
-              "username" -> succeeded.email,
-              "uid" -> succeeded.id.toString
-            )
-          case _ =>
-            Ok(result.toJson)
+    val email = getParam(data, "username").getOrElse("")
+    val password = getParam(data, "password").getOrElse("")
+    val (paramsValid, failedResult) = stringParamsNotEmpty(Seq( email, password)) {
+      parmaErrorResult
+    }
+    if (!paramsValid) {
+      Future { Ok(failedResult.toJson) }
+    } else {
+      val user: User = User(None, email, None, password, None)
+      UserService.login(user) map {
+        result =>
+        if (result.success) {
+          result.data.get match {
+            case succeeded: LoginSucceeded =>
+              Ok(result.toJson).withSession(
+                "username" -> succeeded.email,
+                "uid" -> succeeded.id.toString
+              )
+            case _ =>
+              Ok(result.toJson)
+          }
+        } else {
+          Ok(result.toJson)
         }
-      } else {
-        Ok(result.toJson)
       }
     }
   }
 
   def register = Action.async(parse.urlFormEncoded) {
     implicit request =>
-    println("try register: " + json)
     val data = request.body
-    val uuid = getParam(data, "uuid").get
-    val text = getParam(data, "text").get
-    println(s"captchauuid: $uuid, text: $text")
-    if(!CaptchaController.validate(uuid, text)) {
-      println("captcha validate failed!")
-      Future {Ok(ApiResult(false, -1, "error: 验证码错误").toJson)}
+    val uuid = getParam(data, "uuid").getOrElse("")
+    val text = getParam(data, "text").getOrElse("")
+    val email = getParam(data, "email").getOrElse("")
+    val password = getParam(data, "password").getOrElse("")
+    val nationalId = getParam(data, "nationalId")
+    val realName = getParam(data, "realName")
+    val (paramsValid, failedResult) = stringParamsNotEmpty(Seq(uuid, text, email, password)){
+      parmaErrorResult
+    }
+    if (!paramsValid) {
+      Future { Ok(failedResult.toJson) }
+    }
+    else if(!CaptchaController.validate(uuid, text)) {
+      Future { Ok(ApiResult(false, -1, "error: 验证码错误").toJson) }
     } else {
-      println("captcha validate success!")
-      val email = getParam(data, "email").get
-      val password = getParam(data, "password").get
-      val nationalId = getParam(data, "nationalId")
-      val realName = getParam(data, "realName")
       val user: User = User(Some(-1L), email, realName, password, nationalId)
       UserService.register(user) map {
         result =>
