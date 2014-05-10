fe0031dc433cd8450a41164d85f9900f47ec00ae
Dong Wang <dong77@gmail.com>
2014-05-10 18:47:11 +0800

    SimpleResult -> Result

--- exchange/app/controllers/Authentication.scala
+++ exchange/app/controllers/Authentication.scala
@@ -8,7 +8,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 class AuthenticatedRequest[A](val uid: String, request: Request[A]) extends WrappedRequest[A](request)
 
 object Authenticated extends ActionBuilder[AuthenticatedRequest] {
-  def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[SimpleResult]) = {
+  def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]) = {
     request.session.get("uid").map { uid =>
       block(new AuthenticatedRequest(uid, request))
     } getOrElse {
--- exchange/app/controllers/MainController.scala
+++ exchange/app/controllers/MainController.scala
@@ -92,7 +92,7 @@ object MainController extends Controller with Json4s {
       stream.close()
     }
 
-    SimpleResult(
+    Result(
       header = ResponseHeader(200),
       body = fileContent
     ).withHeaders("Content-type" -> "application/force-download", "Content-Disposition" -> "attachment")
