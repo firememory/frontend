63713a9acd14bf9a7b69003474679aa91c4d5745
ChenXi <cx@coinport.com>
2014-06-18 15:11:51 +0800

    add coin.js

---
+++ exchange/public/js/coin.js
@@ -0,0 +1,45 @@
+var app = angular.module('coinport.coin', ['ui.bootstrap', 'ngResource', 'navbar', 'coinport.app']);
+
+app.controller('CoinTransferCtrl', function ($scope, $http, $window) {
+    $scope.coin = $window.location.pathname.replace("/coin/", "");
+    $scope.addressUrl = COINPORT.addressUrl[$scope.coin];
+
+    $scope.page = 1;
+    $scope.limit = 10;
+    $scope.reloadTransfers = function () {
+        $http.get('/api/' + $scope.coin + '/transfer/-1', {params: {limit: $scope.limit, page: $scope.page}})
+            .success(function (data, status, headers, config) {
+                $scope.transfers = data.data.items;
+                $scope.count = data.data.count;
+            });
+    };
+
+    $scope.reloadTransfers();
+});
+
+app.controller('CoinWalletCtrl', function ($scope, $http, $window) {
+    $scope.coin = $window.location.pathname.replace("/coin/", "");
+    $http.get('/api/account/-1')
+        .success(function (data, status, headers, config) {
+        $scope.accounts = data.data.accounts;
+        });
+
+    $http.get('/api/open/reserve/' + $scope.coin )
+        .success(function(data, status, headers, config) {
+            $scope.reserveAmount = data.data.amount;
+            console.log('reserveAmount', $scope.reserveAmount.display);
+        });
+
+    $scope.hotWallets = [];
+    $scope.coldWallets = [];
+
+    $http.get('/api/open/wallet/' + $scope.coin + '/hot')
+        .success(function(data, status, headers, config) {
+            $scope.hotWallets = data.data;
+        });
+
+    $http.get('/api/open/wallet/' + $scope.coin + '/cold')
+        .success(function(data, status, headers, config) {
+            $scope.coldWallets = data.data;
+        });
+});
