0b4dfa37ebda417f8388df0e9219a419c8902e68
ChenXi <cx@coinport.com>
2014-06-24 18:56:03 +0800

    polish orders page

--- exchange/public/js/orders.js
+++ exchange/public/js/orders.js
@@ -4,6 +4,9 @@ app.controller('OrdersCtrl', function ($scope, $http, $window) {
     $scope.market = $window.location.pathname.replace("/orders/", "");
     $scope.marketdisplay = $scope.market.substr(0,3)+'/'+$scope.market.substr(3,3);
 
+    $scope.subject = $scope.market.substr(0,3);
+    $scope.currency2 = $scope.market.substr(3,3);
+
     $scope.loadOrders = function() {
         $http.get('/api/' + $scope.market + '/orders', {params: {limit: $scope.limit, page: $scope.page}})
             .success(function(data, status, headers, config) {
