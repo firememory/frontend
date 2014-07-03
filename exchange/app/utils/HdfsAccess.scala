4c029e8d2b0f615d206e51aef90a206d5e2c094b
LiuChunming <chunming02@163.com>
2014-07-03 14:31:53 +0800

    support to download account data

--- exchange/app/utils/HdfsAccess.scala
+++ exchange/app/utils/HdfsAccess.scala
@@ -10,8 +10,6 @@ object HdfsAccess {
   val basePath = Play.current.configuration.getString("hdfs.export.dir").getOrElse("/export/")
   val uri = Play.current.configuration.getString("hdfs.uri").getOrElse("hdfs://hadoop:54310")
 
-  println("connecting to hdfs at " + uri)
-
   val conf = new Configuration()
   lazy val hdfs = FileSystem.get(URI.create(uri), conf)
 
@@ -22,6 +20,7 @@ object HdfsAccess {
 
   def listFiles(path: String) = {
     val fullPath = basePath + path
+
     hdfs.listStatus(new Path(fullPath)).map {
       file =>
         FileItem(file.getPath.getName, file.getLen, file.getModificationTime, path)
--- exchange/public/js/coin.js
+++ exchange/public/js/coin.js
@@ -8,6 +8,8 @@ app.controller('CoinCtrl', function ($scope, $http, $window) {
 
     $scope.page = 1;
     $scope.limit = 10;
+    $scope.filesPage = 1;
+    $scope.filesLimit= 5;
 
     $http.get('/api/account/-1000')
         .success(function (data, status, headers, config) {
@@ -41,4 +43,13 @@ app.controller('CoinCtrl', function ($scope, $http, $window) {
     };
 
     $scope.reloadTransfers();
+
+    $scope.loadFiles = function() {
+        $http.get('/api/open/data/csv/asset/'+$scope.coin.toLowerCase(), {params: {limit: $scope.filesLimit, page: $scope.filesPage}})
+        .success(function(data, status, headers, config) {
+            $scope.files = data.data;
+        });
+    };
+
+    $scope.loadFiles();
 });
