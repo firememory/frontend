3b30531bd301ae660f677ca88c28e6bad6fc35bf
Kongliang Zhong <zhongkl@gmail.com>
2014-12-15 22:06:28 +0800

    bug fix in withdrawal page

--- exchange2/public/js/coinport/account.js
+++ exchange2/public/js/coinport/account.js
@@ -224,12 +224,12 @@ app.controller('TransferCtrl', ['$scope', '$http', '$timeout', function ($scope,
             }
         }
 
-        if (emailVerOn && (!$scope.withdrawalData.emailuuid || $scope.withdrawalData.emailuuid == '')) {
+        if ($scope.emailVerOn && (!$scope.withdrawalData.emailuuid || $scope.withdrawalData.emailuuid == '')) {
             $scope.displayWithdrawalError(Messages.transfer.messages['emailNotSend']);
             return;
         }
 
-        if (mobileVerOn && (!$scope.withdrawalData.phoneuuid || $scope.withdrawalData.phoneuuid == '')) {
+        if ($scope.mobileVerOn && (!$scope.withdrawalData.phoneuuid || $scope.withdrawalData.phoneuuid == '')) {
             $scope.displayWithdrawalError(Messages.transfer.messages['smsNotSend']);
             return;
         }
